27472306432e488172f45ee1eb0e912e73cf88f7
mahefasolofo <mahefa.rasolofonirina@gmail.com>
2022-09-11 15:54:32 +0300

    add froont

---
+++ front/src/App.js
@@ -0,0 +1,20 @@
+
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import TrajetPopulaire from './components/TrajetPopulaire';
+import Essai from './components/Essai';
+import Offre from './components/Offre';
+import AvisClient from './components/AvisClient';
+
+
+function App() {
+  return (
+    <div className="super_container">
+      <Navbar/>  
+      <Main />
+      
+    </div>
+  );
+}
+
+export default App;
