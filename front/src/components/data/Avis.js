38cad659acdd046507882fb442440831be4c622d
mahefasolofo <mahefa.rasolofonirina@gmail.com>
2022-09-12 15:02:39 +0300

    accueil divisé en components

---
+++ front/src/components/data/Avis.js
@@ -0,0 +1,68 @@
+
+import img1 from '../../images/test_1.jpg';
+import icn1 from '../../images/kayak.png';
+import img2 from '../../images/test_4.jpg';
+import icn2 from '../../images/backpack.png'
+import img3 from '../../images/test_5.jpg'
+import icn3 from '../../images/island_t.png'
+import img4 from '../../images/people2.jpg'
+import icn4 from '../../images/island_t.png'
+import img5 from '../../images/test_6.jpg'
+import inc5 from '../../images/backpack.png'
+
+
+const avis=[
+    {
+        "id":1,
+        "nom":"Alina PARIS",
+        "date":"24 mai 2022",
+        "titre":"Meilleures vacances de ma vie",
+        "avis":"Mes vacances se sont passés à merveille grace ... aux covoiturages.",
+        "image":img1,
+        "icone":icn1,
+        "note":"rating_r rating_r_5 offers_rating"
+    },
+    {
+        "id":2,
+        "nom":"Magnus ANDERSON",
+        "date":"21 janvier 2022",
+        "titre":"Je suis sérein",
+        "avis":"Grace à l'existence de covoiturages je suis sérein pour mes déplacements journaliers.",
+        "image":img2,
+        "icone":icn2,
+        "note":"rating_r rating_r_4 offers_rating"
+    },
+    {
+        "id":3,
+        "nom":"Michelle RABE",
+        "date":"15 août 2022",
+        "titre":"Ecologique et pratique",
+        "avis":"C'est écologique, il y aura moins de circulation dans la ville. C'est pratique avec son système très intuitif.",
+        "image":img3,
+        "icone":icn3,
+        "note":"rating_r rating_r_5 offers_rating"
+
+    },
+    {   
+        "id":4,
+        "nom":"John RADISON",
+        "date":"05 juillet 2022",
+        "titre":"Pour éviter les bus",
+        "avis":"Même si les contacts humaines ne m'intéressent pas, celà me permet d'éviter les longs trajets en bus.",
+        "image":img4,
+        "icone":icn4,
+        "note":"rating_r rating_r_3 offers_rating"
+    },
+    {   
+        "id":5,
+        "nom":"Leonardo ZORZI",
+        "date":"26 juin 2022",
+        "titre":"Lieu de rencontre",
+        "avis":"C'est plaisant de rencontrer d'autres personnes de même passion que vous pendant le temps d'un voyage.",
+        "image":img5,
+        "icone":inc5,
+        "note":"rating_r rating_r_4 offers_rating"
+    }
+]
+
+export default avis
