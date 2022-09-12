a7c624c4a4de2dea44079ad24dbea29c58646bb0
thaja <thajavola@gmail.com>
2022-09-12 22:00:36 +0300

    react-router

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Home from "./components/Home";
 import Navbar from './components/Navbar/Navbar';
+import Offre from './components/Offre/Offre';
 import Footer from './components/Footer/Footer';
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
@@ -10,7 +12,14 @@ function App() {
     <React.Fragment>
       <div className="super_container">
         <Navbar />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offres" element={<Offre />} />
+          <Route path="/demandes" element={<Home />} />
+          <Route path="/evenements" element={<Home />} />
+          <Route path="/a_propos" element={<Home />} />
+        </Routes>
+
         <Footer />
       </div>
 
