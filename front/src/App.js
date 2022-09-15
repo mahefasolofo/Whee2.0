375bc4380c09835d5903bd6b1ba04758322c9082
Joel <joelandriambola@gmail.com>
2022-09-15 14:57:29 +0300

    dernier modif de la partie front conn

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import 'moment/locale/fr'
-import { Component , useState, useMemo}  from 'react';
+import { Component , useState, useMemo, useEffect, useContext}  from 'react';
 import Home from "./components/Home";
 import Offre from './components/Pages/Offre';
 import Demande from './components/Pages/Demande';
@@ -12,12 +12,42 @@ import { Routes, Route } from "react-router-dom";
 import Connexion from "./components/Pages/Connexion";
 import Inscription from "./components/Pages/Inscription";
 import { UserContext } from './api/UserContext';
+import jwt_decode from "jwt-decode";
 
 
 
 function App() {
-  const [user, setUser] = useState("null");
-    const value = useMemo(()=>({user, setUser}), [user, setUser]);
+  const [value, setValue] = useState("null");
+
+  function handleCredentialResponse(response){
+    console.log("Encode JWT id Token: "+ response.credential);
+    console.log(jwt_decode(response.credential));
+    //setAuth(jwt_decode(response.credential));
+    setValue(jwt_decode(response.credential));
+    document.getElementById('id01').style.display='none'; 
+    localStorage.setItem('token', response.credential);
+    console.log("mety");
+    
+  }
+
+  
+   
+    useEffect(()=>{
+      /* global google */
+      localStorage.setItem('token', "null");
+      google.accounts.id.initialize({
+        client_id: '162247164460-u010auh9f2t4er36klc81sqd7g8elg7u.apps.googleusercontent.com',
+        callback: handleCredentialResponse,
+        
+      });
+      
+    
+      google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        {theme: "outline", size : "large", width:"signInDiv"}
+    
+      );
+    },[] );
 
   return (
     
