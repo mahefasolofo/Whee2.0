d35b7c782450a290045a55a4ccdbab06523d2187
mahefasolofo <mahefa.rasolofonirina@gmail.com>
2022-09-15 17:01:31 +0300

    modification entity event

---
+++ whee_2.0-Back/src/main/java/whee/demo/repository/EventRepository.java
@@ -0,0 +1,10 @@
+package whee.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import whee.demo.entity.Event;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event,Long> {
+
+}
