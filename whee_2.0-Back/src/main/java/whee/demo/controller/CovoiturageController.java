4531363a0c835716a080241e347872b2cc0200d3
mahefasolofo <mahefa.rasolofonirina@gmail.com>
2022-09-16 19:41:20 +0300

    changement images map et database

--- whee_2.0-Back/src/main/java/whee/demo/controller/CovoiturageController.java
+++ whee_2.0-Back/src/main/java/whee/demo/controller/CovoiturageController.java
@@ -1,12 +1,7 @@
 package whee.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import whee.demo.entity.Covoiturage;
 import whee.demo.repository.CovoiturageRepository;
 
@@ -28,4 +23,8 @@ public class CovoiturageController {
     public Covoiturage createAnnonce(@RequestBody Covoiturage annonce ){
         return covoiturageRepository.save(annonce);
     }
+
+    @GetMapping("/findByIdCovoit/{id}")
+    public List<Covoiturage> findByCovoitureur(Long id){return covoiturageRepository.findByCovoitureur(id);}
+    //   ne fonctionne pas encore : Mahefa
 }
--- whee_2.0-Back/src/main/java/whee/demo/repository/CovoiturageRepository.java
+++ whee_2.0-Back/src/main/java/whee/demo/repository/CovoiturageRepository.java
@@ -2,6 +2,7 @@ package whee.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import whee.demo.entity.Covoiturage;
 
@@ -10,8 +11,9 @@ import java.util.List;
 @Repository
 public interface CovoiturageRepository extends JpaRepository<Covoiturage,Long> {
 
-    @Query("SELECT all FROM public.covoiturage WHERE covoitureur_id=")
-    List<Covoiturage> findByCovoitureur();
-   
+
+    @Query(value = "SELECT id FROM public.covoiturage WHERE covoitureur_id= :id", nativeQuery = true)
+    List<Covoiturage> findByCovoitureur(@Param("id")Long id);
+//   ne fonctionne pas encore : Mahefa
 
 }
