fadc01b2b81a36422c50eb7f5853fda24ff28419
Joel <joelandriambola@gmail.com>
2022-09-16 16:36:13 +0300

    joel

--- front/src/App.js
+++ front/src/App.js
@@ -14,6 +14,7 @@ import Inscription from "./components/Pages/Inscription";
 import { UserContext } from './api/UserContext';
 import FormOffre from "./components/PublierOffre/FormOffre";
 import jwt_decode from "jwt-decode";
+import { AccordionButton } from "react-bootstrap";
 
 
 
@@ -30,6 +31,8 @@ function App() {
     document.getElementById('id01').style.display='none'; 
     localStorage.setItem('token', response.credential);
     console.log(jwt_decode(localStorage.getItem('token')));
+    const valeurInter = jwt_decode(localStorage.getItem('token'));
+    
     setUser(jwt_decode(localStorage.getItem('token')));
      
   }
