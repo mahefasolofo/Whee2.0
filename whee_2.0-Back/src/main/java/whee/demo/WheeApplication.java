df0f770892d1a5edc221528449d5f9b66ef93ac0
Tsiry <thajavola.gmail.com>
2022-09-16 17:15:46 +0300

    inscription

--- whee_2.0-Back/src/main/java/whee/demo/WheeApplication.java
+++ whee_2.0-Back/src/main/java/whee/demo/WheeApplication.java
@@ -1,12 +1,12 @@
 package whee.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import whee.demo.entity.User;
-import whee.demo.repository.UserRepository;
+// import whee.demo.entity.User;
+// import whee.demo.repository.UserRepository;
 
-import javax.annotation.PostConstruct;
+// import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class WheeApplication {
@@ -15,18 +15,16 @@ public class WheeApplication {
 		SpringApplication.run(WheeApplication.class, args);
 
 	}
-	@Autowired
-	private UserRepository userRepository;
+	// @Autowired
+	// private UserRepository userRepository;
 
-	@PostConstruct
-	private void postConstruct() {
-		User admin = new User();
-		admin.setNom("joel");
-		admin.setPrenom("joem");
+	// @PostConstruct
+	// private void postConstruct() {
+	// User admin = new User();
+	// admin.setNom("joel");
+	// admin.setPrenom("joem");
 
-		userRepository.save(admin);
-	}
-
-
+	// userRepository.save(admin);
+	// }
 
 }
--- whee_2.0-Back/src/main/java/whee/demo/controller/UserController.java
+++ whee_2.0-Back/src/main/java/whee/demo/controller/UserController.java
@@ -2,24 +2,26 @@ package whee.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import whee.demo.entity.Covoiturage;
+
 import whee.demo.entity.User;
 import whee.demo.repository.UserRepository;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:3001", "http://someserver:3000"}, allowCredentials = "true")
+@CrossOrigin(origins = { "http://localhost:3001", "http://localhost:3000" }, allowCredentials = "true")
 @RestController
 @RequestMapping("/api/whee/")
 public class UserController {
     @Autowired
     UserRepository userRepository;
+
     @GetMapping("/newCompte")
-    public List<User> getAllCompte(){
+    public List<User> getAllCompte() {
         return userRepository.findAll();
     }
+
     @PostMapping("/newCompte")
-    public User createCompte(@RequestBody User user ){
+    public User createCompte(@RequestBody User user) {
         return userRepository.save(user);
     }
 }
