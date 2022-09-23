626c07e04e926f630c568d3622bb88af35b9ee35
Joel <joelandriambola@gmail.com>
2022-09-23 05:05:39 +0300

    Tout marche

--- whee_2.0-Back/src/main/java/whee/demo/controller/InterestController.java
+++ whee_2.0-Back/src/main/java/whee/demo/controller/InterestController.java
@@ -1,10 +1,7 @@
 package whee.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import whee.demo.entity.Interest;
 import whee.demo.entity.User;
 import whee.demo.repository.InterestRepository;
@@ -23,10 +20,9 @@ public class InterestController {
     UserRepository userRepository;
 
     @GetMapping("/getInterest")
-
     public List<Interest> getInterests() {
-
         return interestRepository.findAll();
     }
+
 
 }
